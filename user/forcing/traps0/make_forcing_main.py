4d714b71bad0063728b91443461489378304e50f
Aurora Leeson <auroral@uw.edu>
2022-12-28 19:57:08 -0800

    updated to include 2021 river data

--- user/forcing/traps0/make_forcing_main.py
+++ user/forcing/traps0/make_forcing_main.py
@@ -2,8 +2,8 @@
 This is the main program for making the RIVER and TRAPS forcing file, for the
 updated ROMS
 
-Test on mac in ipython:
-run make_forcing_main.py -g cas6 -r backfill -d 2020.01.01 -f traps0 -test True
+Test on pc in ipython:
+run make_forcing_main.py -g cas6 -r backfill -d 2021.01.01 -f traps0 -test True
 
 """
 
@@ -65,18 +65,20 @@ G = zrfun.get_basic_info(grid_fn, only_G=True)
 # LIVEOCEAN PRE-EXISTING RIVERS
 
 # Load a dataframe with info for rivers to get
-gridname = 'cas6'
-ri_dir = Ldir['LOo'] / 'pre' / 'river' / gridname
+gtag = 'cas6_v3'
+ri_dir = Ldir['LOo'] / 'pre' / 'river' / gtag
 ri_fn = ri_dir / 'river_info.csv'
 ri_df = pd.read_csv(ri_fn, index_col='rname')
 
 # get historical and climatological data files
 year0 = 1980
-year1 = 2020
+year1 = 2021
+clim_temp_year0 = 1980
+clim_temp_year1 = 2020
 # historical and climatological data
 Ldir['Hflow_fn'] = ri_dir / 'Data_historical' / ('ALL_flow_' + str(year0) + '_' + str(year1) + '.p')
 Ldir['Cflow_fn'] = ri_dir / 'Data_historical' / ('CLIM_flow_' + str(year0) + '_' + str(year1) + '.p')
-Ldir['Ctemp_fn'] = ri_dir / 'Data_historical' / ('CLIM_temp_' + str(year0) + '_' + str(year1) + '.p')
+Ldir['Ctemp_fn'] = ri_dir / 'Data_historical' / ('CLIM_temp_' + str(clim_temp_year0) + '_' + str(clim_temp_year1) + '.p')
 
 # get the list of rivers and indices for this grid
 gri_fn = Ldir['grid'] / 'river_info.csv'
